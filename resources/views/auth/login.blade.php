8932d7cf866d3eccfb28de1d951fb09536a3fc37
Jesús Arturo Carmona Aguirre <jessarturo97@gmail.com>
2024-12-04 00:47:52 -0600

    Correccion en login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,78 +1,22 @@
-{{-- <x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-        
-
-        <div class="flex items-center justify-start mt-4">
-            <x-primary-button>
-                    {{ __('Log in') }}
-            </x-primary-button>
-            
-            @if (Route::has('register'))
-                 <a class="underline text-sm m-4 text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
-                    {{ __('Register') }}
-                </a>
-            @endif
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-          
-        </div>
-    </form>
-</x-guest-layout> --}}
-
 <!doctype html>
 <html lang="es">
 
 <head>
+  <meta name="base-url" content="{{ asset('') }}">
   <meta charset="utf-8" />
   <title>Softconnect_ERP</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
   <meta content="Themesbrand" name="author" />
   <!-- App favicon -->
-  <link rel="shortcut icon" href="{{ asset('/skote/assets/images/favicon.ico') }}">
+  <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
 
   <!-- Bootstrap Css -->
-  <link href="{{ asset('/skote/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet"
-    type="text/css" />
+  <link href="{{ asset('assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
   <!-- Icons Css -->
-  <link href="{{ asset('/skote/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+  <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
   <!-- App Css-->
-  <link href="{{ asset('/skote/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+  <link href="{{ asset('assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
@@ -90,7 +34,7 @@
                   </div>
                 </div>
                 <div class="col-5 align-self-end">
-                  <img src="{{ asset('/skote/assets/images/profile-img.png') }}" alt="" class="img-fluid">
+                  <img src="{{ asset('assets/images/profile-img.png') }}" alt="" class="img-fluid">
                 </div>
               </div>
             </div>
@@ -99,7 +43,8 @@
                 <a href="{{ route('login') }}" class="auth-logo-light">
                   <div class="avatar-md profile-user-wid mb-4">
                     <span class="avatar-title rounded-circle bg-light">
-                      <img src="{{ asset('/skote/assets/images/logo-light.svg') }}" alt="" class="rounded-circle" height="34">
+                      <img src="{{ asset('assets/images/logo-light.svg') }}" alt=""
+                        class="rounded-circle" height="34">
                     </span>
                   </div>
                 </a>
@@ -107,7 +52,7 @@
                 <a href="{{ route('login') }}" class="auth-logo-dark">
                   <div class="avatar-md profile-user-wid mb-4">
                     <span class="avatar-title rounded-circle bg-light">
-                      <img src="{{ asset('/skote/assets/images/logo.svg') }}" alt="" class="rounded-circle"
+                      <img src="{{ asset('assets/images/logo.svg') }}" alt="" class="rounded-circle"
                         height="34">
                     </span>
                   </div>
@@ -118,15 +63,17 @@
                   @csrf
                   <div class="mb-3">
                     <label for="email" class="form-label">{{ __('Email') }}</label>
-                    <input name="email" type="email" class="form-control" id="email" value="{{ old('email') }}"
-                      placeholder="Ingresa tu correo electrónico" required autofocus autocomplete="username">
+                    <input name="email" type="email" class="form-control" id="email"
+                      value="{{ old('email') }}" placeholder="Ingresa tu correo electrónico" required autofocus
+                      autocomplete="username">
                   </div>
 
                   <div class="mb-3">
                     <label class="form-label" for="password">{{ __('Password') }}</label>
                     <div class="input-group auth-pass-inputgroup">
                       <input name="password" type="password" id="password" class="form-control" aria-label="Password"
-                        aria-describedby="password-addon" placeholder="Ingresa tu contraseña" required autocomplete="current-password">
+                        aria-describedby="password-addon" placeholder="Ingresa tu contraseña" required
+                        autocomplete="current-password">
                       <button class="btn btn-light" type="button" id="password-addon"><i
                           class="mdi mdi-eye-outline"></i></button>
                     </div>
@@ -152,14 +99,14 @@
   </div>
 
   <!-- JAVASCRIPT -->
-  <script src="{{ asset('/skote/assets/libs/jquery/jquery.min.js') }}"></script>
-  <script src="{{ asset('/skote/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('/skote/assets/libs/metismenu/metisMenu.min.js') }}"></script>
-  <script src="{{ asset('/skote/assets/libs/simplebar/simplebar.min.js') }}"></script>
-  <script src="{{ asset('/skote/assets/libs/node-waves/waves.min.js') }}"></script>
+  <script src="{{ asset('assets/libs/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('assets/libs/metismenu/metisMenu.min.js') }}"></script>
+  <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
+  <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
 
   <!-- App js -->
-  <script src="{{ asset('/skote/assets/js/app.js') }}"></script>
+  <script src="{{ asset('assets/js/app.js') }}"></script>
 </body>
 
 </html>
