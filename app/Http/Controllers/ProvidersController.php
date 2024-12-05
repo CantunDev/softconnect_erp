44ba0a60ecc2f054836917b2a45048445a12a9b7
CantunDev <bernabe@cantun.com>
2024-12-05 11:28:40 -0600

    Modify column action Providers

--- app/Http/Controllers/ProvidersController.php
+++ app/Http/Controllers/ProvidersController.php
@@ -33,47 +33,63 @@ class ProvidersController extends Controller
                     <p class="text-muted mb-0">'.$result->razonsocial.'</p>';
                 
                 })
-                ->addColumn('action', function($result) {
-                    $buttons = [
-                        'view' => [
-                            'title' => 'View',
-                            'class' => 'btn-outline-primary',
-                            'icon' => 'mdi-eye-outline',
-                            'href' => '#'
-                        ],
-                        'edit' => [
-                            'title' => 'Edit',
-                            'class' => 'btn-outline-info',
-                            'icon' => 'mdi mdi-pencil',
-                            'href' => '#'
-                        ],
-                        'delete' => [
-                            'title' => 'Delete',
-                            'class' => 'btn-outline-danger',
-                            'icon' => 'mdi-delete-outline',
-                            'href' => '#jobDelete',
-                            'data-toggle' => 'modal'
-                        ]
-                    ];
-                    $output = '<ul class="list-unstyled hstack gap-1 mb-0">';
-                    foreach ($buttons as $key => $button) {
-                        $output .= sprintf(
-                            '<li data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="%s">
-                                <a href="%s" class="btn btn-sm %s" %s>
-                                    <i class="mdi %s"></i>
-                                </a>
-                            </li>',
-                            $button['title'],
-                            $button['href'],
-                            $button['class'],
-                            isset($button['data-toggle']) ? 'data-bs-toggle="' . $button['data-toggle'] . '"' : '',
-                            $button['icon']
-                        );
-                    }
-                    $output .= '</ul>';
-                
-                    return $output;
+                ->addColumn('action', function ($result){
+                    $opciones = '';
+                        // if (Auth::user()->can('read_operators')){
+                            $opciones .= '<button type="button"  onclick="btnInfo('.$result->id.')" class="btn btn-sm text-info action-icon icon-dual-blue"><i class="mdi mdi-information-outline font-size-18"></i></button>';
+                        // }
+                        // if (Auth::user()->can('update_operators')){
+                            // $opciones .= '<a href="'.route('restaurants.edit', $result->id).'" class="btn btn-sm text-warning action-icon icon-dual-warning p-1"><i class="mdi mdi-pencil font-size-18"></i></a>';
+                            
+                            // $opciones .= '<a href="'.route('restaurants.edit', $result->id).'" class="btn btn-sm text-primary action-icon icon-dual-warning p-1"><i class="mdi mdi-restore font-size-18"></i></a>';
+                        // }
+                        // if (Auth::user()->can('delete_operators')){
+                            // $opciones .= '<button type="button" onclick="btnDelete('.$result->id.')" class="btn btn-sm text-secondary action-icon icon-dual-secondary btnDelete p-1"><i class="mdi mdi-delete-empty font-size-18"></i></button>';
+                            
+                        // }
+                    return $opciones;
                 })
+                // ->addColumn('action', function($result) {
+                //     $buttons = [
+                //         'view' => [
+                //             'title' => 'View',
+                //             'class' => 'btn-outline-primary',
+                //             'icon' => 'mdi-eye-outline',
+                //             'href' => '#'
+                //         ],
+                //         'edit' => [
+                //             'title' => 'Edit',
+                //             'class' => 'btn-outline-info',
+                //             'icon' => 'mdi mdi-pencil',
+                //             'href' => '#'
+                //         ],
+                //         'delete' => [
+                //             'title' => 'Delete',
+                //             'class' => 'btn-outline-danger',
+                //             'icon' => 'mdi-delete-outline',
+                //             'href' => '#jobDelete',
+                //             'data-toggle' => 'modal'
+                //         ]
+                //     ];
+                //     $output = '<ul class="list-unstyled hstack gap-1 mb-0">';
+                //     foreach ($buttons as $key => $button) {
+                //         $output .= sprintf(
+                //             '<li data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="%s">
+                //                 <a href="%s" class="btn btn-sm %s" %s>
+                //                     <i class="mdi %s"></i>
+                //                 </a>
+                //             </li>',
+                //             $button['title'],
+                //             $button['href'],
+                //             $button['class'],
+                //             isset($button['data-toggle']) ? 'data-bs-toggle="' . $button['data-toggle'] . '"' : '',
+                //             $button['icon']
+                //         );
+                //     }
+                //     $output .= '</ul>';
+                
+                //     return $output;
+                // })
                 ->addColumn('purchases', function($result){
                     return $result->purchases->count();
                 
