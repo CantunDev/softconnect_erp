79c0340e64fa2ea1e1d0a2b7a2a821ab1b70e807
CantunDev <bernabe@cantun.com>
2024-12-19 14:50:42 -0600

    Fix in Providers view, middleware database work

--- app/Http/Controllers/ProvidersController.php
+++ app/Http/Controllers/ProvidersController.php
@@ -4,103 +4,39 @@ namespace App\Http\Controllers;
 
 use App\Models\Sfrt\Provider;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Yajra\DataTables\Facades\DataTables;
-use Illuminate\Support\Facades\Config;
-use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class ProvidersController extends Controller
 {
 
     public function index(Request $request)
     {
-        /*INDEX PARA PROVEEDORES */
-        // $providers = Provider::all();
-        // $ip = '192.168.193.226\NATIONALSOFT';
-        // $database = 'softrestaurant10';
-        // Config::set('database.connections.sqlsrv.host', $ip);
-        // Config::set('database.connections.sqlsrv.database', $database);
-        // DB::purge('sqlsrv');
-        // $mes = Carbon::createFromFormat('Y-m', '2024-11')->month; // Mes de no viembre de 2024
-
-        if ($request->ajax()){
+        if ($request->ajax()) {
             $providers = Provider::query();
             return DataTables::of($providers)
                 ->addIndexColumn()
-                ->addColumn('name', function($result){
+                ->addColumn('name', function ($result) {
                     return $name = '
-                    <h5 class="text-truncate font-size-14 mb-1"><a href="javascript: void(0);" class="text-dark">'.$result->nombre.'</a></h5>
-                    <p class="text-muted mb-0">'.$result->razonsocial.'</p>';
-                
+                    <h5 class="text-truncate font-size-14 mb-1"><a href="javascript: void(0);" class="text-dark">' . $result->nombre . '</a></h5>
+                    <p class="text-muted mb-0">' . $result->razonsocial . '</p>';
                 })
-                ->addColumn('action', function ($result){
+                ->addColumn('action', function ($result) {
                     $opciones = '';
-                        // if (Auth::user()->can('read_operators')){
-                            $opciones .= '<button type="button"  onclick="btnInfo('.$result->id.')" class="btn btn-sm text-info action-icon icon-dual-blue"><i class="mdi mdi-information-outline font-size-18"></i></button>';
-                        // }
-                        // if (Auth::user()->can('update_operators')){
-                            // $opciones .= '<a href="'.route('restaurants.edit', $result->id).'" class="btn btn-sm text-warning action-icon icon-dual-warning p-1"><i class="mdi mdi-pencil font-size-18"></i></a>';
-                            
-                            // $opciones .= '<a href="'.route('restaurants.edit', $result->id).'" class="btn btn-sm text-primary action-icon icon-dual-warning p-1"><i class="mdi mdi-restore font-size-18"></i></a>';
-                        // }
-                        // if (Auth::user()->can('delete_operators')){
-                            // $opciones .= '<button type="button" onclick="btnDelete('.$result->id.')" class="btn btn-sm text-secondary action-icon icon-dual-secondary btnDelete p-1"><i class="mdi mdi-delete-empty font-size-18"></i></button>';
-                            
-                        // }
+                    if (Auth::user()->can('read_providers')) {
+                        $opciones .= '<button type="button"  onclick="btnInfo(' . $result->id . ')" class="btn btn-sm text-info action-icon icon-dual-blue"><i class="mdi mdi-information-outline font-size-18"></i></button>';
+                    }
                     return $opciones;
                 })
-                // ->addColumn('action', function($result) {
-                //     $buttons = [
-                //         'view' => [
-                //             'title' => 'View',
-                //             'class' => 'btn-outline-primary',
-                //             'icon' => 'mdi-eye-outline',
-                //             'href' => '#'
-                //         ],
-                //         'edit' => [
-                //             'title' => 'Edit',
-                //             'class' => 'btn-outline-info',
-                //             'icon' => 'mdi mdi-pencil',
-                //             'href' => '#'
-                //         ],
-                //         'delete' => [
-                //             'title' => 'Delete',
-                //             'class' => 'btn-outline-danger',
-                //             'icon' => 'mdi-delete-outline',
-                //             'href' => '#jobDelete',
-                //             'data-toggle' => 'modal'
-                //         ]
-                //     ];
-                //     $output = '<ul class="list-unstyled hstack gap-1 mb-0">';
-                //     foreach ($buttons as $key => $button) {
-                //         $output .= sprintf(
-                //             '<li data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="%s">
-                //                 <a href="%s" class="btn btn-sm %s" %s>
-                //                     <i class="mdi %s"></i>
-                //                 </a>
-                //             </li>',
-                //             $button['title'],
-                //             $button['href'],
-                //             $button['class'],
-                //             isset($button['data-toggle']) ? 'data-bs-toggle="' . $button['data-toggle'] . '"' : '',
-                //             $button['icon']
-                //         );
-                //     }
-                //     $output .= '</ul>';
-                
-                //     return $output;
-                // })
-                ->addColumn('purchases', function($result){
+                ->addColumn('purchases', function ($result) {
                     return $result->purchases->count();
-                
-                }) 
-                ->addColumn('average', function($result){
-                    return round( $result->purchases->avg('total'),2) ?? 0.0;
-                
-                }) 
-                ->rawColumns(['name','action'])
+                })
+                ->addColumn('average', function ($result) {
+                    return round($result->purchases->avg('total'), 2) ?? 0.0;
+                })
+                ->rawColumns(['name', 'action'])
                 ->make(true);
-         }
+        }
         return view('providers.index');
     }
 
@@ -153,6 +89,6 @@ class ProvidersController extends Controller
     {
         $provider = Provider::findOrFail($request->id);
         $provider->delete();
-        return redirect()->route('providers.index');   
+        return redirect()->route('providers.index');
     }
 }
--- resources/views/providers/index.blade.php
+++ resources/views/providers/index.blade.php
@@ -1,126 +1,120 @@
 @extends('layouts.master')
 @section('content')
-        <div class="page-content">
-            <div class="container-fluid">
-            <!-- start page title -->
-                <div class="row">
-                    <div class="col-12">
-                        <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                            <h4 class="mb-sm-0 font-size-18">Proveedores</h4>
-
-                            <div class="page-title-right">
-                                <ol class="breadcrumb m-0">
-                                    <li class="breadcrumb-item"><a href="javascript: void(0);">Lista</a></li>
-                                    <li class="breadcrumb-item active">Proveedores</li>
-                                </ol>
-                            </div>
-
-                        </div>
+    @component('components.breadcrumb')
+        @slot('title')
+            Proveedores
+        @endslot
+        @slot('bcPrevText')
+            Proveedores
+        @endslot
+        @slot('bcPrevLink')
+            {{ route('providers.index') }}
+        @endslot
+        @slot('bcActiveText')
+            Listado
+        @endslot
+    @endcomponent
+    <div class="card">
+        <div class="card-body border-bottom">
+            <div class="d-flex align-items-center">
+                <h5 class="mb-0 card-title flex-grow-1">Lista de proveedores </h5>
+                @can('create_providers')
+                    <div class="flex-shrink-0">
+                        <a href="{{ route('providers.create') }}" class="btn btn-primary">Nuevo</a>
                     </div>
-                </div>
-                <!-- end page title -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="card">
-                            <div class="card-body border-bottom">
-                                <div class="d-flex align-items-center">
-                                    <h5 class="mb-0 card-title flex-grow-1">Lista de proveedores</h5>
-                                    <div class="flex-shrink-0">
-                                        <a href="#!" class="btn btn-primary">Nuevo</a>
-                                        {{-- <a href="#!" class="btn btn-light"><i class="mdi mdi-refresh"></i></a> --}}
-                                    </div>
-                                </div>
-                            </div>
-                            {{-- <div class="card-body border-bottom">
-                                <div class="row g-3">
-                                    <div class="col-xxl-4 col-lg-6">
-                                        <input type="search" class="form-control" id="searchTableList" placeholder="Search for ...">
-                                    </div>
-                                    <div class="col-xxl-2 col-lg-6">
-                                        <select class="form-select" id="idStatus" aria-label="Default select example">
-                                            <option value="all">Status</option>
-                                            <option value="Active">Active</option>
-                                            <option value="New">New</option>
-                                            <option value="Close">Close</option>
-                                        </select>
-                                    </div>
-                                    <div class="col-xxl-2 col-lg-4">
-                                        <select class="form-select" id="idType" aria-label="Default select example">
-                                            <option value="all">Select Type</option>
-                                            <option value="Full Time">Full Time</option>
-                                            <option value="Part Time">Part Time</option>
-                                        </select>
-                                    </div>
-                                    <div class="col-xxl-2 col-lg-4">
-                                        <div id="datepicker1">
-                                            <input type="text" class="form-control" placeholder="Select date" data-date-format="dd M, yyyy" data-date-container='#datepicker1' data-date-autoclose="true" data-provide="datepicker">
-                                        </div><!-- input-group -->
-                                    </div>
-                                    <div class="col-xxl-2 col-lg-4">
-                                        <button type="button" class="btn btn-soft-secondary w-100" onclick="filterData();"><i class="mdi mdi-filter-outline align-middle"></i> Filter</button>
-                                    </div>
-                                </div>
-                            </div> --}}
-                            <div class="card-body">
-                                <div class="table-responsive">
-                                    <table id="table_invoices" class="table align-middle dt-responsive nowrap w-100 table-check" id="job-list">
-                                        <thead>
-                                            <tr>
-                                                <th scope="col">#</th>
-                                                <th scope="col">Proveedore</th>
-                                                <th scope="col" class="px-4 py-3">Categoria</th>
-                                                <th scope="col" class="px-4 py-3">Compras</th>
-                                                <th scope="col" class="px-4 py-3">Promedio</th>
-                                                <th scope="col" class="px-4 py-3">Credito</th>
-                                                <th scope="col" class="px-4 py-3"></th>
-                                            </tr>
-                                        </thead>
-                                    </table>
-                                    <!-- end table -->
-                                </div>
-                                <!-- end table responsive -->
-                            </div>
-                            <!-- end card body -->
-                        </div><!--end card-->
-                    </div><!--end col-->
-                </div><!--end row-->
-            </div> 
+                @endcan
+            </div>
         </div>
-        
+        <div class="card-body">
+            <div class="table-responsive">
+                <table id="table_providers" class="table align-middle dt-responsive nowrap w-100 table-check" id="job-list">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Proveedore</th>
+                            <th scope="col" class="px-4 py-3">Categoria</th>
+                            <th scope="col" class="px-4 py-3">Compras</th>
+                            <th scope="col" class="px-4 py-3">Promedio</th>
+                            <th scope="col" class="px-4 py-3">Credito</th>
+                            <th scope="col" class="px-4 py-3"></th>
+                        </tr>
+                    </thead>
+                </table>
+                <!-- end table -->
+            </div>
+            <!-- end table responsive -->
+        </div>
+    </div>
 @endsection
 
 @section('js')
-<script>
-    $(document).ready(function(){
-        $('#table_invoices').DataTable({
-            processing: true,
-            serverSide: true,
-            paging: true,
-            ajax: {
+    <script>
+        $(document).ready(function() {
+            $.ajax({
                 url: '{!! route('providers.index') !!}',
-            },
-            columns: [
-                {data: 'DT_RowIndex', name: 'DT_RowIndex', orderable: false, searchable: false},
-                {data: 'name', name: 'name', orderable: false, searchable: false},
-                {data: 'idtipoproveedor', name: 'idtipoproveedor ', orderable: false, searchable: false},
-                {data: 'purchases', name: 'purchases', orderable: false, searchable: false},
-                {data: 'average', name: 'average', orderable: false, searchable: false,render:function(data){return '$'+data}},
-                {data: 'credito', name: 'credito', orderable: false, searchable: false},
-                {data: 'action', name: 'action', orderable: false, searchable: false}
-
-                //  {data: 'fecha', name: 'fecha', orderable: false, searchable: false},
-                //  {data: 'sfrtFolioInvoice', name: 'sfrtFolioInvoice', orderable: false, searchable: false},
-                //  {data: 'sfrtCustomer', name: 'sfrtCustomer', orderable: false, searchable: false},
-                //  {data: 'sfrtCustomerEmail', name: 'sfrtCustomerEmail', orderable: false, searchable: false},
-                //  {data: 'subtotal', name: 'subtotal', orderable: false, searchable: false, render:function(data){return '$'+data}},
-                //  {data: 'impuesto', name: 'impuesto', orderable: false, searchable: false, render:function(data){return '$'+data}},
-                //  {data: 'total', name: 'total', orderable: false, searchable: false, render:function(data){return '$'+data}},
-                //  {data: 'formapago', name: 'formapago', orderable: false, searchable: false},
-                //  {data: 'idmetodopago_SAT', name: 'idmetodopago_SAT', orderable: false, searchable: false},
-                //  {data: 'nota', name: 'nota', orderable: false, searchable: false},
-                // //  {data: 'nota', name: 'nota', orderable: false, searchable: false}
-            ],
+                type: 'GET',
+                success: function(response) {
+                    if (response.data) {
+                        $('#table_providers').DataTable({
+                            processing: true,
+                            serverSide: true,
+                            paging: true,
+                            data: response.data,
+                            columns: [{
+                                    data: 'DT_RowIndex',
+                                    name: 'DT_RowIndex',
+                                    orderable: false,
+                                    searchable: false
+                                },
+                                {
+                                    data: 'name',
+                                    name: 'name',
+                                    orderable: false,
+                                    searchable: false
+                                },
+                                {
+                                    data: 'idtipoproveedor',
+                                    name: 'idtipoproveedor ',
+                                    orderable: false,
+                                    searchable: false
+                                },
+                                {
+                                    data: 'purchases',
+                                    name: 'purchases',
+                                    orderable: false,
+                                    searchable: false
+                                },
+                                {
+                                    data: 'average',
+                                    name: 'average',
+                                    orderable: false,
+                                    searchable: false,
+                                    render: function(data) {
+                                        return '$' + data
+                                    }
+                                },
+                                {
+                                    data: 'credito',
+                                    name: 'credito',
+                                    orderable: false,
+                                    searchable: false
+                                },
+                                {
+                                    data: 'action',
+                                    name: 'action',
+                                    orderable: false,
+                                    searchable: false
+                                }
+                            ],
+                        });
+                    } else {
+                        console.error('No se encontraron datos en la respuesta');
+                    }
+                },
+                error: function(error) {
+                    console.error('Error al cargar los datos:', error);
+                }
+            });
         });
-    });
-</script>
+    </script>
 @endsection
