b33e33398293f5159098712efbce325c7e80bd30
CantunDev <bernabe@cantun.com>
2025-02-03 16:06:18 -0600

    conexion

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -3,12 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class InfoController extends Controller
 {
     public function index(Request $request)
     {
-        return view('info');
-    }
+        $serverName = "192.168.193.29\\NATIONALSOFT"; // Usa doble barra invertida
+        $connectionInfo = [
+            "Database" => "softrestaurant10",
+            "UID" => "sa",
+            "PWD" => "National09",
+            "CharacterSet" => "UTF-8",
+            "TrustServerCertificate" => true // Desactiva la validación del certificado
+        ];
 
+        $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+        if ($conn) {
+            echo "Conexión exitosa";
+        } else {
+            echo "Error en la conexión: ";
+            print_r(sqlsrv_errors());
+        }
+    }
 }
--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -4,8 +4,8 @@ $serverName = "192.168.193.29\NATIONALSOFT";
 $connectionInfo = array("Database" => "softrestaurant10", "UID"=>"sa", "PWD"=>"National09");
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 if (!$conn) {
-    die(print_r(sqlsrv_errors(), true))
+    die(print_r(sqlsrv_errors(), true));
 }else {
-    echo"Conexion lista";
+    echo "Conexion lista";
 }
 ?>
