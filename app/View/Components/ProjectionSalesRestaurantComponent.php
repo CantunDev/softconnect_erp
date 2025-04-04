19fd81888a326af12c6a5826edb0dc568605b87f
CantunDev <bernabe@cantun.com>
2025-04-04 13:30:50 -0600

    Update alcance projections to day

--- app/View/Components/ProjectionSalesRestaurantComponent.php
+++ app/View/Components/ProjectionSalesRestaurantComponent.php
@@ -166,7 +166,8 @@ class ProjectionSalesRestaurantComponent extends Component
          $check_avg = $chequeData['chequePromedio'] ?? 0;
 
         $dailySalesGoal = ($projection['projected_sales'] / $date->getDaysInMonth())  *  $date->getDaysPassed();
-        $salesGoalToDate = $projection['projected_tax'] != 0  ? ($projection['projected_sales'] / $projection['projected_tax']) * 100 : 0;
+        // {{ $alcance_vta = $meta_al_dia != 0 ? ($vta_real / $meta_al_dia) * 100 : '0' }}
+        $salesGoalToDate = $dailySalesGoal != 0  ? ($sales_total / $dailySalesGoal) * 100 : 0;
         $diffProyectionGoal =  /*$chequeData['total']*/ $sales_total != 0 ?( $sales_total - $salesGoalToDate ): 0;
         $salesDeficit =  100 - $salesGoalToDate ;
         $goals_daily = $projection['projected_sales'] != 0 ? ($projection['projected_sales'] / $date->getDaysInMonth()): 0; 
